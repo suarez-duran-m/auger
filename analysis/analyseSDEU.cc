37c68ef30a7e218f6613044027bed02994560c7b
Mauricio Suárez-Durán <mauricio.suarez@unipamplona.edu.co>
2021-01-27 10:24:31 -0500

    analyseSDEU.cc cleaned. New directory, getRms, for rms calculation.

--- analysis/analyseSDEU.cc
+++ analysis/analyseSDEU.cc
@@ -69,34 +69,8 @@ int main (int argc, char *argv[]) {
   TH1F spmth("spmth","Traces for SPMT High",2047,0,2048);
   TH1F ssdpmth("ssdpmth","Traces for SSDPMT High",2047,0,2048);
 
-  TH1F pmt0rl("pmt0rl","RMS base line PMT0 Low", 2047, 0, 2048);
-  TH1F pmt1rl("pmt1rl","RMS base line PMT1 Low", 2047, 0, 2048);
-  TH1F pmt2rl("pmt2rl","RMS base line PMT2 Low", 2047, 0, 2048);
-  TH1F spmtrl("spmtrl","RMS base line SPMT Low", 2047, 0, 2048);
-  TH1F ssdpmtrl("ssdpmtrl","RMS base line SSDPMT Los", 2047, 0, 2048);
-
-  TH1F pmt0rh("pmt0rh","RMS base line PMT0 High", 2047, 0, 2048);
-  TH1F pmt1rh("pmt1rh","RMS base line PMT1 High", 2047, 0, 2048);
-  TH1F pmt2rh("pmt2rh","RMS base line PMT2 High", 2047, 0, 2048);
-  TH1F spmtrh("spmtrh","RMS base line SPMT High", 2047, 0, 2048);
-  TH1F ssdpmtrh("ssdpmtrh","RMS base line SSDPMT Los", 2047, 0, 2048);
-
-
-  double rms0l, rms1l, rms2l, rmssl, rmsssdl;
-  double rms0h, rms1h, rms2h, rmssh, rmsssdh;
-
-  rms0l = rms0h = 0;
-  rms1l = rms1h = 0;
-  rms2l = rms2h = 0;
-  rmssl = rmssh = 0;
-  rmsssdl = rmsssdh =0;
-
-
   TCanvas c1("c1","test",0,0,3600,2400);
   c1.Divide(2,5);
-
-  TCanvas c2("c2","test",0,0,3600,2400);
-  c2.Divide(2,5);
 
   unsigned int nrEvents = 0;
   unsigned int nrEventsRead = 0;
@@ -146,46 +120,18 @@ int main (int argc, char *argv[]) {
           //for (int k=0;k<event.Stations[i].UFadc->NSample;k++) {
           for (int k=0;k<event.Stations[i].UFadc->NSample;k++) {
             //cout << k << endl;
-             
-            rms0l = event.Stations[i].UFadc->GetValue(0,1,k);
-            rms1l = event.Stations[i].UFadc->GetValue(1,1,k);
-            rms2l = event.Stations[i].UFadc->GetValue(2,1,k);
-            rmssl = event.Stations[i].UFadc->GetValue(3,1,k);
-            rmsssdl = event.Stations[i].UFadc->GetValue(4,1,k);
 
-            rms0h = event.Stations[i].UFadc->GetValue(0,0,k);
-            rms1h = event.Stations[i].UFadc->GetValue(1,0,k);
-            rms2h = event.Stations[i].UFadc->GetValue(2,0,k);
-            rmssh = event.Stations[i].UFadc->GetValue(3,0,k);
-            rmsssdh = event.Stations[i].UFadc->GetValue(4,0,k);
+            pmt0l.Fill( k, event.Stations[i].UFadc->GetValue(0,1,k) );
+            pmt1l.Fill( k, event.Stations[i].UFadc->GetValue(1,1,k) ); 
+            pmt2l.Fill( k, event.Stations[i].UFadc->GetValue(2,1,k) );
+            spmtl.Fill( k, event.Stations[i].UFadc->GetValue(3,1,k) );
+            ssdpmtl.Fill( k, event.Stations[i].UFadc->GetValue(4,1,k) );
 
-            pmt0l.Fill( k, rms0l );
-            pmt1l.Fill( k, rms1l ); 
-            pmt2l.Fill( k, rms2l );
-            spmtl.Fill( k, rmssl );
-            ssdpmtl.Fill( k, rmsssdl );
-
-            pmt0h.Fill( k, rms0h );
-            pmt1h.Fill( k, rms1h );
-            pmt2h.Fill( k, rms2h );
-            spmth.Fill( k, rmssh );
-            ssdpmth.Fill( k, rmsssdh );
-
-            /*
-            if ( k < 100 ){
-              pmt0rl.Fill(rms0l);
-              pmt1rl.Fill(rms1l);
-              pmt2rl.Fill(rms2l);
-              spmtrl.Fill(rmssl);
-              ssdpmtrl.Fill(rmsssdl);
-            
-              pmt0rh.Fill(rms0h);
-              pmt1rh.Fill(rms1h);
-              pmt2rh.Fill(rms2h);
-              spmtrh.Fill(rmssh);
-              ssdpmtrh.Fill(rmsssdh);
-            }
-            */
+            pmt0h.Fill( k, event.Stations[i].UFadc->GetValue(0,0,k) );
+            pmt1h.Fill( k, event.Stations[i].UFadc->GetValue(1,0,k) );
+            pmt2h.Fill( k, event.Stations[i].UFadc->GetValue(2,1,k) );
+            spmth.Fill( k, event.Stations[i].UFadc->GetValue(3,1,k) );
+            ssdpmth.Fill( k, event.Stations[i].UFadc->GetValue(4,1,k) );
 
             //for (int j=0;j<5;j++) // Run on type of PMT
               //cout << " " << event.Stations[i].UFadc->GetValue(j,0,k) << " " // 0 for high; 1 for low
@@ -218,7 +164,7 @@ int main (int argc, char *argv[]) {
         TString nameEvent = TString::UItoa(event.Id, 10);
         TString nameStati = TString::UItoa(event.Stations[i].Id, 10);
 
-        c1.Print("plots/dec2020/"+nameEvent+"-"+nameStati+".pdf");
+        c1.Print("plots/jan2021/"+nameEvent+"-"+nameStati+".pdf");
         
         pmt0l.Reset();
         pmt1l.Reset();
@@ -234,30 +180,5 @@ int main (int argc, char *argv[]) {
       }
     }
   }
-/*
-  c2.cd(1);
-  pmt0rl.Draw();
-  c2.cd(2);
-  pmt0rh.Draw();
-  c2.cd(3);
-  pmt1rl.Draw();
-  c2.cd(4);
-  pmt1rh.Draw();
-  c2.cd(5);
-  pmt2rl.Draw();
-  c2.cd(6);
-  pmt2rh.Draw();
-  c2.cd(7);
-  spmtrl.Draw();
-  c2.cd(8);
-  spmtrh.Draw();
-  c2.cd(9);
-  ssdpmtrl.Draw();
-  c2.cd(10);
-  ssdpmtrh.Draw();
-
-  c2.Print("rms.pdf");
-  */
-
   return 0;
 }
